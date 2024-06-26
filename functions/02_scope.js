c312ab82442149ac9a31c302cb1726f77d239e8f
luffytaro1070 <devanshugupta721@gmail.com>
2024-06-26 13:06:20 +0530

    hell

---
+++ functions/02_scope.js
@@ -0,0 +1,9 @@
+var c = 300
+if(true){
+    let a=10
+    const b=20
+    var c=50
+}
+// console.log(a)
+// console.log(b)
+console.log(c)//even though c's value is 300 and in loop it should be changing temporarily,still the value of c becomes 50,that's why var is crap 
