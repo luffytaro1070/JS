1e472130b31e8005f43103545d77f3f917836942
luffytaro1070 <devanshugupta721@gmail.com>
2024-06-26 15:19:34 +0530

    hhh

--- functions/02_scope.js
+++ functions/02_scope.js
@@ -7,3 +7,13 @@ if(true){
 // console.log(a)
 // console.log(b)
 console.log(c)//even though c's value is 300 and in loop it should be changing temporarily,still the value of c becomes 50,that's why var is crap 
+
+function one(){
+    const username="hitest"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    two()
+}
+one()
